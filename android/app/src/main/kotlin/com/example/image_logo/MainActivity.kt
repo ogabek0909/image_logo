6f25d2e6d34e2eead4b75d9402528565e995075c
ZARIFMAC <uzbzarif@gmail.com>
2022-09-20 15:31:34 +0500

    init

---
+++ android/app/src/main/kotlin/com/example/image_logo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.image_logo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LoginPage());
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: Container(),
+    ));
+  }
+}
